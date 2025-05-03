d07e6b09fc6f26b19b387aa1b8a7883b44358297
mehd-io <mehdi@mehd.io>
2025-05-03 19:37:58 +0100

    update aerospace/sketchybar config

--- sketchybar/plugins/space_windows.sh
+++ sketchybar/plugins/space_windows.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
 if [ "$SENDER" = "aerospace_workspace_change" ]; then
-  # Handle previous workspace
-  if [ -n "$PREV_WORKSPACE" ]; then
-    prevapps=$(aerospace list-windows --workspace "$PREV_WORKSPACE" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
-    sketchybar --set space.$PREV_WORKSPACE drawing=on
-    if [ "${prevapps}" != "" ]; then
-      icon_strip=" "
-      while read -r app; do
-        icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
-      done <<<"${prevapps}"
-      sketchybar --set space.$PREV_WORKSPACE label="$icon_strip"
-    else
-      sketchybar --set space.$PREV_WORKSPACE label=""
-    fi
-  fi
-
-  # Handle current workspace
-  if [ -n "$FOCUSED_WORKSPACE" ]; then
-    apps=$(aerospace list-windows --workspace "$FOCUSED_WORKSPACE" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
-    sketchybar --set space.$FOCUSED_WORKSPACE drawing=on
+  # Get all visible workspaces
+  visible_workspaces=$(aerospace list-workspaces --format "%{id} %{workspace-is-visible}" | grep "true" | awk '{print $1}')
+  
+  # Update only visible workspaces
+  for workspace in $visible_workspaces; do
+    is_focused=$(aerospace list-workspaces --format "%{id} %{workspace-is-focused}" | grep "^$workspace " | awk '{print $2}')
+    apps=$(aerospace list-windows --workspace "$workspace" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
+    
+    sketchybar --set space.$workspace drawing=on
+    
     if [ "${apps}" != "" ]; then
       icon_strip=" "
       while read -r app; do
         icon_strip+=" $($CONFIG_DIR/plugins/icon_map_fn.sh "$app")"
       done <<<"${apps}"
-      sketchybar --set space.$FOCUSED_WORKSPACE label="$icon_strip"
+      sketchybar --set space.$workspace label="$icon_strip"
     else
-      sketchybar --set space.$FOCUSED_WORKSPACE label=""
+      sketchybar --set space.$workspace label=""
     fi
-  fi
+  done
 fi
