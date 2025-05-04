3845729d5224d72ded18ea1d793374e85a8ee2fc
mehd-io <mehdi@mehd.io>
2025-05-04 10:03:00 +0100

    adapt path on aerospace/sketchybar install

--- install_aerospace.sh
+++ install_aerospace.sh
@@ -1,5 +1,7 @@
 brew install --cask nikitabobko/tap/aerospace
 mkdir -p $HOME/.config/aerospace
-cp -r $HOME/.config/aerospace/aerospace.toml $HOME/.config/aerospace_backup
-mv aerospace.toml $HOME/.config/aerospace/aerospace.toml
+if [ -f "$HOME/.config/aerospace/aerospace.toml" ]; then
+  cp $HOME/.config/aerospace/aerospace.toml $HOME/.config/aerospace_backup
+fi
+ln -sf "$PWD/aerospace/aerospace.toml" "$HOME/.config/aerospace/aerospace.toml"
 
--- install_sketchybar.sh
+++ install_sketchybar.sh
@@ -6,6 +6,8 @@ brew tap FelixKratz/formulae
 brew install sketchybar
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.23/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
-cp -r $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
-mv sketchybar $HOME/.config/sketchybar
+if [ -d "$HOME/.config/sketchybar" ]; then
+  cp -r $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
+fi
+cp -r sketchybar $HOME/.config/sketchybar
 brew services restart sketchybar
