d6a66479b6da286f946bc82a2f902db62da3fdb3
mehd-io <mehdi@mehd.io>
2025-05-04 10:32:07 +0100

    use brew to install font for sketchybar

--- install_sketchybar.sh
+++ install_sketchybar.sh
@@ -4,7 +4,7 @@ brew install jq
 brew install gh
 brew tap FelixKratz/formulae
 brew install sketchybar
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.23/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+brew install --cask font-hack-nerd-font
 
 if [ -d "$HOME/.config/sketchybar" ]; then
   cp -r $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
