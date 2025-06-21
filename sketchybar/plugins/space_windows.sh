c876a2fd16f36dec09b8769d01a84e8fb5aebaff
Carlos <37572598+carloros@users.noreply.github.com>
2025-06-21 17:41:57 +0200

    Update space_windows.sh for multiple screen setups

--- sketchybar/plugins/space_windows.sh
+++ sketchybar/plugins/space_windows.sh
@@ -22,3 +22,35 @@ if [ "$SENDER" = "aerospace_workspace_change" ]; then
     fi
   done
 fi
+
+# Assigning spaces to monitors dynamically for multiple screen setups
+monitor_index=1
+for monitor in $(aerospace list-monitors --format "%{monitor-appkit-nsscreen-screens-id}"); do
+  for sid in $(aerospace list-workspaces --monitor "$monitor"); do
+    sketchybar --add item space.$sid left \
+      --set space.$sid display="$monitor_index" \
+      --subscribe space.$sid aerospace_workspace_change \
+      --set space.$sid \
+      drawing=on \
+      background.color=0x44ffffff \
+      background.corner_radius=5 \
+      background.drawing=on \
+      background.border_color=0xAAFFFFFF \
+      background.border_width=0 \
+      background.height=25 \
+      icon="$sid" \
+      icon.padding_left=10 \
+      icon.shadow.distance=4 \
+      icon.shadow.color=0xA0000000 \
+      label.font="sketchybar-app-font:Regular:16.0" \
+      label.padding_right=20 \
+      label.padding_left=0 \
+      label.y_offset=-1 \
+      label.shadow.drawing=off \
+      label.shadow.color=0xA0000000 \
+      label.shadow.distance=4 \
+      click_script="aerospace workspace $sid" \
+      script="$CONFIG_DIR/plugins/aerospace.sh $sid"
+  done
+  monitor_index=$((monitor_index + 1))
+done
